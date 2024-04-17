dd12449d57f8a09f706d6d57e068dfc9226f3940
Hoàng Trường <truong.ptit.0812@gmail.com>
2024-04-17 22:09:13 +0700

    UserService

---
+++ microservice/UserService/src/main/java/com/example/userservice/response/LoginResponse.java
@@ -0,0 +1,49 @@
+package com.example.userservice.response;
+
+
+import com.example.userservice.entity.User;
+
+public class LoginResponse {
+    private String message;
+    private Boolean status;
+    private User user;
+
+    public LoginResponse(String message, Boolean status, User user) {
+        this.message = message;
+        this.status = status;
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginResponse{" +
+                "message='" + message + '\'' +
+                ", status=" + status +
+                ", user=" + user +
+                '}';
+    }
+}
