939ae3a4a20753f031dbebfee5e463c65b826bb8
Hoàng Trường <truong.ptit.0812@gmail.com>
2024-04-19 02:11:05 +0700

    UserService

--- microservice/UserService/src/main/java/com/example/userservice/controller/UserController.java
+++ microservice/UserService/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,18 +1,14 @@
 package com.example.userservice.controller;
 
 
-
 import com.example.userservice.dto.LoginDTO;
 import com.example.userservice.dto.UserDTO;
-import com.example.userservice.entity.User;
 import com.example.userservice.response.LoginResponse;
 import com.example.userservice.response.RegisterResponse;
 import com.example.userservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -36,12 +32,6 @@ public class UserController {
     public ResponseEntity<?> loginUser(@RequestBody LoginDTO loginDTO) {
         LoginResponse loginResponse = userService.loginUser(loginDTO);
         return ResponseEntity.ok(loginResponse);
-    }
-
-    @GetMapping(path = "/role")
-    public String roleUser(@RequestHeader("userId") int userId) {
-        Optional<User> user = userService.findById(userId);
-        return user.get().getRole();
     }
 
 
