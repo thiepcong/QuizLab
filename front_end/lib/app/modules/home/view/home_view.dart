156d1015678a6499a346df3e822c45f379759944
thiepcong <thiepcong12@gmail.com>
2024-05-07 23:32:21 +0700

    update 7/5:final 2

--- front_end/lib/app/modules/home/view/home_view.dart
+++ front_end/lib/app/modules/home/view/home_view.dart
@@ -125,44 +125,48 @@ class _HomeViewState extends State<HomeView> {
                 child: Stack(
                   children: [
                     state.quizes.isNotEmpty
-                        ? Column(
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.3,
-                                child: TextFormField(
-                                  controller: _controller,
-                                  inputFormatters: [
-                                    LengthLimitingTextInputFormatter(6),
-                                  ],
-                                  validator: (value) {
-                                    if (value == null || value.length != 6) {
-                                      return "Vui lòng nhập đúng 6 ký tự";
-                                    }
-                                    return null;
-                                  },
-                                  decoration: InputDecoration(
-                                      hintText: 'Nhập mã tham gia',
-                                      suffixIcon: TextButton(
-                                        onPressed: () => cubit
-                                            .getTestFromCode(_controller.text),
-                                        child: const Text("Tham gia"),
-                                      ),
-                                      border: const OutlineInputBorder(
-                                          borderSide: BorderSide())),
+                        ? SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.3,
+                                  child: TextFormField(
+                                    controller: _controller,
+                                    inputFormatters: [
+                                      LengthLimitingTextInputFormatter(6),
+                                    ],
+                                    validator: (value) {
+                                      if (value == null || value.length != 6) {
+                                        return "Vui lòng nhập đúng 6 ký tự";
+                                      }
+                                      return null;
+                                    },
+                                    decoration: InputDecoration(
+                                        hintText: 'Nhập mã tham gia',
+                                        suffixIcon: TextButton(
+                                          onPressed: () =>
+                                              cubit.getTestFromCode(
+                                                  _controller.text),
+                                          child: const Text("Tham gia"),
+                                        ),
+                                        border: const OutlineInputBorder(
+                                            borderSide: BorderSide())),
+                                  ),
                                 ),
-                              ),
-                              ...state.quizes.map(
-                                (e) => QuizItem(
-                                  item: e,
-                                  onStart: () => context
-                                      .pushRoute(CreateTestViewRoute(item: e)),
-                                  onSave: () => cubit.saveExcel(e),
-                                  onDelete: () => cubit.deleteQuiz(e),
-                                  onEdit: () => context
-                                      .pushRoute(EditQuizViewRoute(item: e)),
-                                ),
-                              )
-                            ],
+                                ...state.quizes.map(
+                                  (e) => QuizItem(
+                                    item: e,
+                                    onStart: () => context.pushRoute(
+                                        CreateTestViewRoute(item: e)),
+                                    onSave: () => cubit.saveExcel(e),
+                                    onDelete: () => cubit.deleteQuiz(e),
+                                    onEdit: () => context
+                                        .pushRoute(EditQuizViewRoute(item: e)),
+                                  ),
+                                )
+                              ],
+                            ),
                           )
                         : Center(
                             child: SizedBox(
